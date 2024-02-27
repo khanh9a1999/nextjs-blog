8712017c4aa9bfca685c529be499e67b19bcfb8d
khanhtq99 <khanhtq@bap.jp>
2024-02-27 14:22:42 +0700

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,70 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+import { QrReader } from 'react-qr-reader'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+      <div>
+      {/* <Spin spinning={loading} indicator={<SiteLoading width={50} />} /> */}
+      <div className='title'>Scan QR Code</div>
+      <QrReader
+        constraints={{ facingMode: 'environment' }}
+        scanDelay={1000}
+        ViewFinder={() => <>123</>}
+        onResult={(result, error) => {
+          if (result) {
+            console.log('false')
+          }
+
+          if (error) {
+            console.log('false')
+          }
+        }}
+      />
+      <div className='close-btn'>
+        {/* <Button onClick={() => setVisible(false)}>Close</Button> */}
+      </div>
+    </div>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
